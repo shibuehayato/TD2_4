44204cd1a76a1e79b77a65a2801eac2d5e4b0312
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2024-04-05 16:40:08 +0900

    ちょっと調整した

--- RightArrow.cpp
+++ RightArrow.cpp
@@ -10,7 +10,7 @@ void RightArrow::Initialize(Model* model)
 	worldTransform_.Initialize();
 
 	
-	worldTransform_.translation_ = { 6.0f,0.0f,35.0f };
+	worldTransform_.translation_ = { 6.0f,0.0f,34.0f };
 	worldTransform_.rotation_ = { 0.0f,-1.6f,0.0f };
 	velocity_ = { 1.0f,1.0f,1.0f };
 
